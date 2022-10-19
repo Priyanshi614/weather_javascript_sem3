8cc16f6266f6b91c6e59f3e387d5ef1356e5a67b
Priyanshi614 <priyanshibhadresha3@gmail.com>
2022-10-19 16:11:00 +0530

    first commit

---
+++ main1.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const homeFile = fs.readFileSync("index1.html" , "utf-8");
+const server = http.createServer((req , res) => {
+    if(req.)
+
+});
